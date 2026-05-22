b66e62e8552fc57062f4966eafc45c5a7c6e4b3b
unknown <axolotlsforme@gmail.com>
2026-05-22 17:18:08 -0400

    Integrated modular option 3 posts system

---
+++ Posts.js
@@ -0,0 +1,12 @@
+const myPosts = [
+    {
+        date: "MAY 22, 2026",
+        title: "WELCOME TO THE NEW BLOG AREA!",
+        content: "Hey guys! I just updated the site so I can add quick text updates right here by editing a simple list file on GitHub. Let me know what you think!"
+    },
+    {
+        date: "MAY 21, 2026",
+        title: "FOREST+ UPDATE COMING SOON",
+        content: "Working on some brand new custom models for the Forest+ map overhaul. Stay tuned for the release!"
+    }
+];
