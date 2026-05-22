b29ab1698bf4833b80c091472e7abe44e2cf839f
PotTheMan <axolotlsforme@gmail.com>
2026-05-22 17:33:35 -0400

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,12 +1,7 @@
 const myPosts = [
     {
         date: "MAY 22, 2026",
-        title: "WELCOME TO THE NEW BLOG AREA!",
-        content: "Hey guys! I just updated the site so I can add quick text updates right here by editing a simple list file on GitHub. Let me know what you think!"
-    },
-    {
-        date: "MAY 21, 2026",
-        title: "FOREST+ UPDATE COMING SOON",
-        content: "Working on some brand new custom models for the Forest+ map overhaul. Stay tuned for the release!"
+        title: "WELCOME TO POTTHESITE!",
+        content: "I MADE FOR FUN, AND TO HAVE AN EASIER WAY TO SHOW OFF UPCOMING PROJECTS."
     }
 ];
